5a3c7c29c3f5bb78202a2542dbfd51cc65c963f4
jc979 <jc979@cornell.edu>
2018-04-28 21:43:00 -0400

    getting rid of unnecessary stuff

--- src/todo/routes.py
+++ src/todo/routes.py
@@ -9,41 +9,4 @@ import json
 @app.route('/favorites', methods=['GET'])
 def get_controller():
   return jsonify(db.get_favorites())
-
-# @app.route('/', methods=['GET'])
-# def index():
-#   return jsonify({"HELLO": "WORLD"})
-
-# @app.route('/tasks/all', methods=['DELETE'])
-# def delete_all_controller(): 
-#   db.delete_all_task()
-#   return jsonify({"success": "true"})
-
-
-
-# @app.route('/tasks', methods=['POST', 'GET', 'DELETE'])
-# def post_controller():
-
-#   if request.method == 'POST':
-#     name = request.args['name']
-#     description = request.args['description']
-#     tags = request.args['tags']
-#     due_date = request.args['due_date']
-
-#     task = Task(due_date, description, tags, name)
-#     db.insert_task_table(task)
-
-#     return jsonify(task.to_dict())
-
-#   if request.method == 'GET':
-#     if request.args.get('id'): 
-#       id  = request.args.get('id')
-#       return jsonify(db.get_task(id))
-#     print "getting here"
-#     return jsonify(db.get_all_tasks())
-
-#   if request.method == 'DELETE':
-#     if request.args.get('id'): 
-#       id  = request.args.get('id')
-#       db.delete_task(id)
-#     return jsonify({"success": "true"}) 
+    
